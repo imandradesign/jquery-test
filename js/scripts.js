47c85ce44e1724c9d5016a616af4b233f2fc08d0
Imandra McKenzie and Jennifer Marks <imandra@>
2019-02-04 19:29:33 -0800

    Add colors to javascript

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,19 @@ $(document).ready(function() {
   $("#slideclick").click(function() {
     $("#slideup").slideToggle();
   });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
 });
